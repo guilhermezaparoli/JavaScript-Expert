a0f411539cc347676b8fdce333c3bb42d2c8d971
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-19 11:22:37 -0300

    tests of empty files, five items, invalid header and three items

--- modulo01/aula01-mocks/index.test.js
+++ modulo01/aula01-mocks/index.test.js
@@ -18,8 +18,33 @@ const assert = require("assert")
 }
 {
     const filePath = "./mocks/five-items-invalid.csv"
-    const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE)
+    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
     const result = File.csvToJSON(filePath)
     await assert.rejects(result, expected)
+}
+{
+    const filePath = "./mocks/three-items-valid.csv"
+    const expected = [
+        {
+          id: 1,
+          name: "xuxa da silva",
+          profession: "developer",
+          age: 120
+        },
+        {
+          id: 2,
+          name: "jose da silva",
+          profession: "manager",
+          age: 30
+        },
+        {
+          id: 3,
+          name: "zezin",
+          profession: "QA",
+          age: 25
+        },
+      ]
+    const result = await File.csvToJSON(filePath)
+     assert.deepEqual(result, expected)
 }
 })()
